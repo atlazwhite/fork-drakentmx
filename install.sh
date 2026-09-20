4fdff55bfdcffa35b6a0d8d088f9153a35f3073a
joakonoqui-cmd <joakonoqui-cmd@users.noreply.github.com>
2026-09-20 17:00:30 -0300

    instalacion de sh y requirements

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Instalando drakenTMX.."
+echo ""
+echo "Actualizando paquetes"
+pkg update -y && pkg upgrade -y
+
+echo "instalando paquetes"
+pkg install python ffmpeg git -y
+
+echo "instalando dependencias" 
+
+pip install -r requirements.txt
+
+termux-setup-storage
+
+echo "Todo listo, ejecute: python descarga.py "
+
+echo "para cualquier error, hacerselo saber al dueño del repositorio"
